bbaf64a51607a99ee7385575aa1e36a8ec43e1a5
dbrowner1 <dbrowner1@ggc.edu>
2024-04-09 20:16:04 -0400

    init commit

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+class IllegalTriangleSideException extends Exception {
+    public IllegalTriangleSideException(String message) {
+        super(message);
+    }
+}
+
+class Triangle {
+    private double side1;
+    private double side2;
+    private double side3;
+
+    public Triangle(double side1, double side2, double side3) {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+
+    public boolean checkSides() throws IllegalTriangleSideException {
+        if (side1 + side2 <= side3 || side2 + side3 <= side1 || side1 + side3 <= side2) {
+            throw new IllegalTriangleSideException("Incorrect triangle sides");
+        }
+        return true;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Triangle triangle1 = new Triangle(3, 4, 5);
+            System.out.println("Triangle 1: " + triangle1.checkSides());
+
+            Triangle triangle2 = new Triangle(1, 1, 3);
+            System.out.println("Triangle 2: " + triangle2.checkSides());
+
+            Triangle triangle3 = new Triangle(1, 2, 3);
+            System.out.println("Triangle 3: " + triangle3.checkSides());
+        } catch (IllegalTriangleSideException e) {
+            System.out.println("Exception caught: " + e.getMessage());
+        }
+    }
+}
